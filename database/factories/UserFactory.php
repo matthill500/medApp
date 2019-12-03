8554ff62ca733aa20834bd552681a16b34b499cc
Matthew Hill <matthill500@hotmail.com>
2019-12-03 14:09:07 +0000

    factory complete

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => 'secret', // password
+        'password' => bcrypt('secret'), // password
         'remember_token' => Str::random(10),
     ];
 });
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,42 +18,40 @@ class UsersTableSeeder extends Seeder
 
       factory(App\User::class,10)->create();
 
-      $roles = App\Role::all();
-
-
-      App\User::all()->each(function ($user) use ($roles) {
-      $user->roles()->attach(
-          $roles->random(rand(1, $roles->count()))
-      );
+      App\User::all()->each(function ($user){
+      $user->roles()->attach(rand(1, Role::count()));
     });
+        $role_admin = Role::where('name', 'admin')->first();
+        $role_doctor = Role::where('name', 'doctor')->first();
+        $role_patient = Role::where('name', 'patient')->first();
 
-       // $admin = new User();
-       // $admin->name = 'Matt Hill';
-       // $admin->email = 'admin@medapp.ie';
-       // $admin->password = bcrypt('secret');
-       // $admin->save();
-       // $admin->roles()->attach($role_admin);
-       //
-       // $doctor = new User();
-       // $doctor->name = 'John Jones';
-       // $doctor->email = 'johnj@medapp.ie';
-       // $doctor->password = bcrypt('secret');
-       // $doctor->save();
-       // $doctor->roles()->attach($role_doctor);
-       //
-       // $patient = new User();
-       // $patient->name = 'Jenny';
-       // $patient->email = 'jenny@medapp.ie';
-       // $patient->password = bcrypt('secret');
-       // $patient->save();
-       // $patient->roles()->attach($role_patient);
-       //
-       // $doctor = new User();
-       // $doctor->name = 'Ben';
-       // $doctor->email = 'ben@medapp.ie';
-       // $doctor->password = bcrypt('secret');
-       // $doctor->save();
-       // $doctor->roles()->attach($role_doctor);
+        $admin = new User();
+        $admin->name = 'Matt Hill';
+        $admin->email = 'admin@medapp.ie';
+        $admin->password = bcrypt('secret');
+        $admin->save();
+        $admin->roles()->attach($role_admin);
+
+         $doctor = new User();
+         $doctor->name = 'John Jones';
+         $doctor->email = 'johnj@medapp.ie';
+         $doctor->password = bcrypt('secret');
+         $doctor->save();
+         $doctor->roles()->attach($role_doctor);
+
+         $patient = new User();
+         $patient->name = 'Jenny';
+         $patient->email = 'jenny@medapp.ie';
+         $patient->password = bcrypt('secret');
+         $patient->save();
+         $patient->roles()->attach($role_patient);
+
+         $doctor = new User();
+         $doctor->name = 'Ben';
+         $doctor->email = 'ben@medapp.ie';
+         $doctor->password = bcrypt('secret');
+         $doctor->save();
+         $doctor->roles()->attach($role_doctor);
 
     }
 }
